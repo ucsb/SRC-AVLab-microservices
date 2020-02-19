a76affd39a60c4dcbc20a3a1696b863cfe54890b
Jean Huang <49555723+jinghuangtw@users.noreply.github.com>
2020-02-19 15:24:04 -0800

    Update embedimg_jean.py
    Update the script for running in the right repository structure, and for overwriting the old mp3 file.

--- embedimg_jean.py
+++ embedimg_jean.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import os.path
 import sys
 import argparse
 import subprocess
@@ -14,28 +15,49 @@ def main():
 	'''
 	do the thing
 	'''
+	# audioDir = '/Users/specpa7/Desktop/Jean_python/audio'
+	# imgDir = '/Users/specpa7/Desktop/Jean_python/img/'
+	# outputDir = '/Users/specpa7/Desktop/Jean_python/output'
 
-	for audioFile in os.listdir('/Users/specpa7/Desktop/Jean_python/audio'):
-		audioFileName, ext = os.path.splitext(audioFile)
-		audioDir = os.path.join('/Users/specpa7/Desktop/Jean_python/audio',audioFile)
+	for file in os.listdir('/Users/specpa7/Desktop/Jean_python/TestFiles'):
+		audioDir = '/Users/specpa7/Desktop/Jean_python/TestFiles/' + file + '/' + file + 'd' + '.mp3'
+		imgDir = '/Users/specpa7/Desktop/Jean_python/TestFiles/' + file + '/' + file + '.jpg'
+		outputDir = '/Users/specpa7/Desktop/Jean_python/TestFiles/' + file + '/' + file + 'd' + '_embedded' + '.mp3'
 		
-		for imgFile in os.listdir('/Users/specpa7/Desktop/Jean_python/img'):
-			imgFileName, ext = os.path.splitext(imgFile)
-			imgDir = os.path.join('/Users/specpa7/Desktop/Jean_python/img',imgFile)
+		# Check if the corresponding img exists
+		isFile = os.path.isfile(imgDir)
+		# Check if the mp3 is already updated
+		#isEmbedded = os.path.isfile(outputDir)
 
-			if audioFileName == imgFileName and audioFileName != '.DS_Store':
-				# Define the directory of the output file
-				outputDir = os.path.join('/Users/specpa7/Desktop/Jean_python/output',audioFile)
+		if isFile: 	
 
-				# Embed the img and the audio	
-				subprocess.call(['ffmpeg', '-i', audioDir, '-i', imgDir, '-map', '0:0', '-map', '1:0', '-c', 'copy', '-id3v2_version', '3', '-metadata:s:v', 'title="Album cover"', '-metadata:s:v', 'comment="Cover (front)"', outputDir])
+			# Embed the img and the audio	
+			subprocess.call(['ffmpeg', '-i', audioDir, '-i', imgDir, '-map', '0:0', '-map', '1:0', '-c', 'copy', '-id3v2_version', '3', '-metadata:s:v', 'title="Album cover"', '-metadata:s:v', 'comment="Cover (front)"','-y', audioDir])
+		
+		else: 
+			print("No corresponding cover photo found for ", file)
 
-				# delete the old files
-				# os.remove(audioDir)
-				# os.remove(imgDir)
+	# for audioFile in os.listdir(audioDir):
+	# 	audioFileName, ext = os.path.splitext(audioFile)
+	# 	audioPath = os.path.join(audioDir,audioFile)
+		
+	# 	imgPath = imgDir + audioFileName + '.jpg'
+	# 	isFile = os.path.isfile(imgPath)
+		
 
-			else: 
-				print("No corresponding cover photo found!")
+	# 	if isFile: 	
+	# 		# Define the directory of the output file
+	# 		outputPath = os.path.join(outputDir,audioFile)
+
+	# 		# Embed the img and the audio	
+	# 		subprocess.call(['ffmpeg', '-i', audioPath, '-i', imgPath, '-map', '0:0', '-map', '1:0', '-c', 'copy', '-id3v2_version', '3', '-metadata:s:v', 'title="Album cover"', '-metadata:s:v', 'comment="Cover (front)"', outputPath])
+
+	# 		# delete the old files
+	# 		# os.remove(audioDir)
+	# 		# os.remove(imgDir)
+
+	# 	else: 
+	# 		print("No corresponding cover photo found for ", audioFileName)
 
 		
 
