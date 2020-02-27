13aec5bc4f24bb4227169d9869b1e9e693727044
Jean Huang <49555723+jinghuangtw@users.noreply.github.com>
2020-02-27 14:41:13 -0800

    Update embedimg_jean.py
    - Fix the directory structure issue.
    - Fix the ffmpeg replacing issue that causes file damaged.
    - Replace old mp3 with the old one.

--- embedimg_jean.py
+++ embedimg_jean.py
@@ -10,15 +10,13 @@ import pyodbc
 import config as rawconfig
 import util as ut
 import logger as log
+from shutil import move
 
 def main():   
 	'''
 	do the thing
 	'''
-	# audioDir = '/Users/specpa7/Desktop/Jean_python/audio'
-	# imgDir = '/Users/specpa7/Desktop/Jean_python/img/'
-	# outputDir = '/Users/specpa7/Desktop/Jean_python/output'
-
+	
 	for file in os.listdir('/Users/specpa7/Desktop/Jean_python/TestFiles'):
 		audioDir = '/Users/specpa7/Desktop/Jean_python/TestFiles/' + file + '/' + file + 'd' + '.mp3'
 		imgDir = '/Users/specpa7/Desktop/Jean_python/TestFiles/' + file + '/' + file + '.jpg'
@@ -27,15 +25,27 @@ def main():
 		# Check if the corresponding img exists
 		isFile = os.path.isfile(imgDir)
 		# Check if the mp3 is already updated
-		#isEmbedded = os.path.isfile(outputDir)
+		isEmbedded = os.path.isfile(outputDir)
 
-		if isFile: 	
+		if isFile and not isEmbedded:
 
 			# Embed the img and the audio	
-			subprocess.call(['ffmpeg', '-i', audioDir, '-i', imgDir, '-map', '0:0', '-map', '1:0', '-c', 'copy', '-id3v2_version', '3', '-metadata:s:v', 'title="Album cover"', '-metadata:s:v', 'comment="Cover (front)"','-y', audioDir])
+			subprocess.call(['ffmpeg', '-i', audioDir, '-i', imgDir, '-map', '0:0', '-map', '1:0', '-c', 'copy', '-id3v2_version', '3', '-metadata:s:v', 'title="Album cover"', '-metadata:s:v', 'comment="Cover (front)"', outputDir])
 		
 		else: 
 			print("No corresponding cover photo found for ", file)
+
+	for file in os.listdir('/Users/specpa7/Desktop/Jean_python/TestFiles'):
+		audioDir = '/Users/specpa7/Desktop/Jean_python/TestFiles/' + file + '/' + file + 'd' + '.mp3'
+		outputDir = '/Users/specpa7/Desktop/Jean_python/TestFiles/' + file + '/' + file + 'd' + '_embedded' + '.mp3'
+		
+		# Check if the corresponding img exists
+		isFile = os.path.isfile(outputDir)
+
+		if isFile:
+			# Replace the files
+			move(outputDir, audioDir)
+
 
 	# for audioFile in os.listdir(audioDir):
 	# 	audioFileName, ext = os.path.splitext(audioFile)
